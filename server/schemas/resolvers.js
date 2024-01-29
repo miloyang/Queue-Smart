a0c9a4161ba7920c669cc1dc5c7f15b22c61c944
M Yang <miloyang9@gmail.com>
2024-01-29 14:22:03 +1100

    updated schemas in Server

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,52 @@
+//! Import dependencies
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+// Create resolver object
+const resolvers = {
+  // Query resolvers
+  Query: {
+    // Find logged in user
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          '-__v -password'
+        );
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
+
+  // Mutation resolvers
+  Mutation: {
+    // add a new user
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // check if user is logged in
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+// Export resolvers
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,18 +1,51 @@
 // Import the gql tagged template function;
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String!
-    venues: [Venue]
-}
+    password: String!
+    venues: Venue
+  }
 
-#! JWT
-type Auth {
-  token: ID!
-  user: User
-}`
+  type Venue {
+    _id: ID
+    venueName: String!
+    queues: [Queue]!
+  }
 
-module.exports = typeDefs
+  type Queue {
+    _id: ID!
+    customerName: String!
+    customerMobile: String!
+    createdAt: String!
+  }
+
+  #! JWT
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User!]!
+    user(username: String!): User
+    venue(id: ID!): Venue
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addVenue(venueName: String!): Venue!
+    removeVenue(venueId: ID!): Venue
+    addQueue(
+      customerName: String!
+      customerMobile: String!
+      venueId: ID!
+    ): Queue!
+  }
+`;
+
+module.exports = typeDefs;

ffda7cab689519e6c5687d98b61ec68348f8f4b6
M Yang <miloyang9@gmail.com>
2024-02-06 16:40:15 +1100

    added .mpmrc file and updated build to dist

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.use(express.json());
 
 // If we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   // Catch-all route where any route that isnt defined is treated as a 404 error
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
 
